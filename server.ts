603597d542fc68e5f6adda2da07d7f95b5bb2ab5
Curtis Larson <quackware@gmail.com>
2023-12-24 09:17:44 -0500

    feat: add simple index file so we can perform uptime checks

--- server.ts
+++ server.ts
@@ -7,9 +7,22 @@ const EMPTY_PATHS = ["", "/"];
 const QUACKWARE_NO_MOD_REDIRECT = !!Deno.env.get("QUACKWARE_NO_MOD_REDIRECT");
 const QUACKWARE_MOD_REDIRECT_FILENAME = "mod.ts";
 
+const indexFile = await Deno.readTextFile(new URL("./public/index.html", import.meta.url));
+const favicon = await Deno.readFile(new URL("./public/favicon.ico", import.meta.url));
+
 serve((req) => {
   const url = new URL(req.url);
-  const [, repo, ...rest] = url.pathname.split("/");
+
+  const { pathname } = url;
+
+  if (pathname === "/favicon.ico") {
+    return new Response(favicon);
+  }
+  if (pathname === "/") {
+    return new Response(indexFile, { headers: { "Content-Type": "text/html" } });
+  }
+
+  const [, repo, ...rest] = pathname.split("/");
 
   if (EMPTY_PATHS.includes(repo)) {
     console.log(`Returning error for no repo`);
@@ -20,10 +33,14 @@ serve((req) => {
 
   if (EMPTY_PATHS.includes(path)) {
     if (QUACKWARE_NO_MOD_REDIRECT) {
-      console.log(`Returning error for path due to QUACKWARE_NO_MOD_REDIRECT: '${path}'`);
+      console.log(
+        `Returning error for path due to QUACKWARE_NO_MOD_REDIRECT: '${path}'`,
+      );
       return new Response(null, { status: 404 });
     } else {
-      console.log(`Path '${path}' for repo '${repo}' force redirect to '${QUACKWARE_MOD_REDIRECT_FILENAME}'`);
+      console.log(
+        `Path '${path}' for repo '${repo}' force redirect to '${QUACKWARE_MOD_REDIRECT_FILENAME}'`,
+      );
       path = QUACKWARE_MOD_REDIRECT_FILENAME;
     }
   }
